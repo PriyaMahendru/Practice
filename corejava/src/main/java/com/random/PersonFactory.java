dd73edacc6971b644fae66bfd4cad6993a13050a
PriyaMahendru <39409190+PriyaMahendru@users.noreply.github.com>
2019-05-28 08:10:09 +0530

    Incorported below comments
     RandomPerson.java --> should be PersonFactory.java
       - All predefined list(ages,names,isHumanValues{should be humanTypes}) should be at class level
       - I didn't see the use of parameterized constructor of Person
       - variable name as 'r' and 'n' is not acceptable. should be as 'random' and 'person'
       - Person parameterized constructor must be used here as
         Person person= new Person(random.getRandomValue(humanTypes),random.getRandomValue(names),random.getRandomValue(ages));
         randomPersonList.add(person);
       - Reporting function doesn't have any input as I want to pass criteria then get the result. it's fixed now and there is no further use for new input
       - method getNameWitha() should be filterPersonByName(List<Person> persons,String name_criteria)    - to be done
       - method getAgeMoreThanFifty() should be filterPersonByAgegeaterThan(List<Person> persons,int age_criteria)
       - method getAllHuman() should be filterPersonByHumanFlag(List<Person> persons,boolean humantype_criteria)
       - There should be a reporting class which holds these methods (can/should be as static method).
       - main method for loop should be move to a method called createRandomPersonList(int number_of_person)
       - number_of_person can be assigned from runtime if not there then assign 10
       - call all there function to print the reporting inside the main method(which is still missing)
       
    - Random.java method getRandomNumber() --> should be getRandomValue() as it returns string, boolean and number
    - Person.java is good
    - add some docs for complex logic/ class and method (for all)

---
+++ corejava/src/main/java/com/random/PersonFactory.java
@@ -0,0 +1,35 @@
+package com.random;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class PersonFactory {
+	
+	static List<Integer> ages=Arrays.asList(21,22,23,24,25,56,57,58,595,50,51,52,53,54,55,26,27,58,59,50,51,52,43,54,55,51);
+	static List<String> names=Arrays.asList("priya","shivam","himanshu","riya","rajat","kirtika","ashima","bikram","anil","jestin","raj","krantika","jaya","priyanka","shiv","ayush","rita","kanchan","kirti","aneesha","vikram","arun","mathew","surbhi","sandarbh","joy");
+	static List<Boolean> humanTypes=Arrays.asList(true,false);
+	static  List<Person> randomPersonList=new ArrayList<Person>();
+	
+	public static List<Person> createRandomPersonList(int number_of_person)
+	{
+		Random random=new Random();
+         if(number_of_person==0)
+         {
+        	 number_of_person=10;
+         }
+		for(int i=0;i<number_of_person;i++)
+		{
+			Person person=new Person(random.getRandomValue(humanTypes),random.getRandomValue(ages),random.getRandomValue(names));
+			randomPersonList.add(person);
+		}		
+		
+		return randomPersonList;		
+	}
+	
+	
+	
+	
+	}
+
